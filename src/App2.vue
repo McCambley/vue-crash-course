af292c478c9998a81d6c53bcb925ac5eee6c4fa1
McCambley <jakemccamb16@gmail.com>
2025-10-24 17:16:55 -0400

    style: format code in App.vue and App2.vue for consistency

--- src/App2.vue
+++ src/App2.vue
@@ -1,26 +1,23 @@
 <script>
-
 export default {
   data() {
     return {
-      name: "Jake Mccambley ",
-      status: "pending",
+      name: 'Jake Mccambley ',
+      status: 'pending',
       tasks: ['task 1', 'task 2', 'task 3'],
-      link: "https://example.com"
+      link: 'https://example.com',
     }
   },
   methods: {
     toggleStatus() {
       this.status = 'active'
-    }
-  }
+    },
+  },
 }
-
 </script>
 
-
 <template>
-  <h1>{{name}}</h1>
+  <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
@@ -30,7 +27,7 @@ export default {
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
   <!-- <a v-bind:href="link">Click for Google</a> -->
-   <a :href="link">Google Link</a>
-   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-   <button @click="toggleStatus">Change Status</button>
-</template> 
+  <a :href="link">Google Link</a>
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
