68ed513f9fb0599bcb40fd0215c022a81bdd0c1f
McCambley <jakemccamb16@gmail.com>
2025-10-24 17:11:24 -0400

    refactor: move options api to App2

---
+++ src/App2.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default {
+  data() {
+    return {
+      name: "Jake Mccambley ",
+      status: "pending",
+      tasks: ['task 1', 'task 2', 'task 3'],
+      link: "https://example.com"
+    }
+  },
+  methods: {
+    toggleStatus() {
+      this.status = 'active'
+    }
+  }
+}
+
+</script>
+
+
+<template>
+  <h1>{{name}}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+   <a :href="link">Google Link</a>
+   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+   <button @click="toggleStatus">Change Status</button>
+</template> 
