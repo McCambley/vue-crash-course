ae055b1d5337630656bf041014216521ecfb846d
McCambley <jakemccamb16@gmail.com>
2025-10-25 11:51:00 -0400

    refactor: update heading style in App.vue and clean up main.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   variants: {
     extend: {},
   },
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
