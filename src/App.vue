1cc76edf625e68023ebe7661f5d5a63757213b98
McCambley <jakemccamb16@gmail.com>
2025-10-25 16:29:04 -0400

    feat: add Hero component with title and subtitle props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
+import Hero from './components/Hero.vue'
 </script>
 
 <template>
   <NavBar />
+  <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
 </template>
