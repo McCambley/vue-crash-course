bc6aee619d3d93ecb6066218ab759c0eae45b2f5
McCambley <jakemccamb16@gmail.com>
2025-10-26 17:14:21 -0400

    feat: add vue-router and create HomeView component for routing and job listings display

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-import Hero from './components/Hero.vue'
-import HomeCards from './components/HomeCards.vue'
-import JobListings from './components/JobListings.vue'
+import { RouterView } from 'vue-router'
 import 'primeicons/primeicons.css'
 </script>
 
 <template>
   <NavBar />
-  <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
-  <HomeCards />
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
