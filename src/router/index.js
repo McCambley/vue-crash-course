18bb72728bcb63369c417747407fe62ad3457fb7
McCambley <jakemccamb16@gmail.com>
2025-10-26 18:11:58 -0400

    feat: update Job routing and implement JobView component with loading state

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HomeView from '@/views/HomeView.vue'
 import JobsView from '@/views/Jobs.vue'
 import Jobs from '@/views/Jobs.vue'
 import NotFound from '@/views/NotFound.vue'
-import Job from '@/views/Job.vue'
+import Job from '@/views/JobView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
