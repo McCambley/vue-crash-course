732f3373acbac46943d6933d80cf4267b1d8d3c0
McCambley <jakemccamb16@gmail.com>
2025-10-26 17:46:33 -0400

    feat: refactor JobListings component to use reactive state and improve loading handling

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { ref, defineProps, onMounted } from 'vue'
+import { ref, reactive, defineProps, onMounted } from 'vue'
 import jobData from '@/jobs.json'
 import JobListing from './JobListing.vue'
 import { RouterLink } from 'vue-router'
 
-const jobs = ref([])
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+})
 
 defineProps({
   limit: Number,
@@ -19,9 +22,11 @@ onMounted(async () => {
     const response = await fetch('http://localhost:8000/jobs')
     const data = await response.json()
 
-    jobs.value = data
+    state.jobs = data
   } catch (error) {
     console.error(error)
+  } finally {
+    state.isLoading = false
   }
 })
 </script>
@@ -31,7 +36,11 @@ onMounted(async () => {
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+        <JobListing
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
       </div>
     </div>
   </section>
