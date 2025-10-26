4d830a031891f390fdfb63ff79e8141552774f47
McCambley <jakemccamb16@gmail.com>
2025-10-26 11:19:56 -0400

    feat: add JobListings and JobListing components with job data for displaying job opportunities

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import NavBar from '@/components/NavBar.vue'
 import Hero from './components/Hero.vue'
 import HomeCards from './components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
 </script>
 
 <template>
   <NavBar />
   <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
   <HomeCards />
+  <JobListings />
 </template>
